f389a6dd3c38d91b82fb5ce05fe0930c1942b8ea
ayylemao <matthias.vogler@bluewin.ch>
2023-05-29 20:31:23 +0200

    did some stuff, changed model architecture

--- main.py
+++ main.py
@@ -3,104 +3,153 @@ from game import Game
 from model import CoolModel
 import torch as th
 import matplotlib.pyplot as plt
-
+import random
 
 model = CoolModel()
 
 loss_arr = []
-for i in range(0, 1000):
+for i in range(0, 10000):
     game = Game()
     while True:
+        # Player 2
+        #state_input = th.tensor(game.state).unsqueeze(dim=0).unsqueeze(dim=0).float()
+        #model_out = model(state_input*(-1))
+
+        #admissable_tens = th.tensor(game.admissable_moves)
+        #nonzero_indices = th.nonzero(admissable_tens)
+        #masked_tensor = model_out[0, nonzero_indices]
+        #max_index = nonzero_indices[th.argmax(masked_tensor)]
+        #choice = max_index.item()
+        #game.move(choice, -1)
+
+        # RANDOM PLAYER
+        nonzero_indices = np.nonzero(game.admissable_moves)[0] 
+        game.move(np.random.choice(nonzero_indices), -1) 
+        
+        if game.concluded:
+            if game.outcome == 1: 
+                winner = 'Random Player'
+            elif game.outcome == 0:
+                winner = 'Draw'
+                print('Draw')
+            break
+        # =============================================================================
+
         # Player 1
         state_input = th.tensor(game.state).unsqueeze(dim=0).unsqueeze(dim=0).float()
         model_out = model(state_input)
-        choice = (th.tensor(game.admissable_moves) * model_out).argmax().item()
+
+        # Since model_out[i] can be zero but the only admissable move,
+        # we need to be smarter about choosing the next move:
+        admissable_tens = th.tensor(game.admissable_moves)
+        nonzero_indices = th.nonzero(admissable_tens)
+        masked_tensor = model_out[0, nonzero_indices]
+        max_index = nonzero_indices[th.argmax(masked_tensor)]
+        choice = max_index.item()
+
         game.move(choice, 1)
         if game.concluded:
             if game.outcome == 1: 
-                pass
+                winner = 'Neural Network'
             elif game.outcome == 0:
-                print('Draw')
-            break
-        # Player 2
-        state_input = th.tensor(game.state).unsqueeze(dim=0).unsqueeze(dim=0).float()
-        model_out = model(state_input*(-1))
-        choice = (th.tensor(game.admissable_moves) * model_out).argmax().item()
-        game.move(choice, -1)
-        if game.concluded:
-            if game.outcome == 1: 
-                pass
-            elif game.outcome == 0:
+                winner = 'Draw'
                 print('Draw')
             break
 
 
-
-    criterion = th.nn.MSELoss(reduction='sum')
-    optimizer = th.optim.Adam(model.parameters(), lr=0.0001)
+    criterion = th.nn.MSELoss()
+    optimizer = th.optim.SGD(model.parameters(), lr=0.0001)
+    player_id = np.empty(42)
+    player_id[::2] = -1
+    player_id[1::2] = 1
     if game.outcome == 1:
         loss_sum = 0.0
-        for turn in range(game.turn_counter, 2, -2):
+        for turn in range(0, game.turn_counter, 2):
             # WINNER        
             # y_true 
-            current_move = game.move_history[turn-1]
-
-            y =  np.zeros(7)
-            y[current_move[0]] = 1
-            y = th.tensor(y).unsqueeze(dim=0).unsqueeze(dim=0).unsqueeze(dim=0).float()
-
-            # y_pred 
-            current_state = th.tensor(game.history[turn-2]).unsqueeze(dim=0).unsqueeze(dim=0).float()
-            y_pred = model(current_state)
-
-            loss = criterion(y_pred, y)
+            current_move = game.move_history[turn]
+            if winner == 'Random Player':
+                # Winner prop
+                current_move = game.move_history[turn]
+                y =  np.zeros(7)
+                y[current_move[0]] = 0.9
+                y = th.tensor(y).unsqueeze(0).float()
+                current_state = th.tensor(game.history[turn]*(-1)).unsqueeze(dim=0).unsqueeze(dim=0).float()
+                y_pred = model(current_state)
+                loss = criterion(y_pred, y) 
+                loss_sum += loss.item() 
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                # Loser prop
+                try:
+                    current_move = game.move_history[turn+1]
+                    y =  np.ones(7) * 0.7
+                    y[current_move[0]] = 0.2
+                    y = th.tensor(y).unsqueeze(0).float()
+                    current_state = th.tensor(game.history[turn+1]).unsqueeze(dim=0).unsqueeze(dim=0).float()
+                    y_pred = model(current_state)
+                    loss = criterion(y_pred, y) 
+                    loss_sum += loss.item() 
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                except IndexError:
+                    pass
+            elif winner == 'Draw':
+                pass 
             
-            loss_sum += loss.item()
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            # LOSER
-            # y_true 
-            #current_move = game.move_history[turn-2]
-
-            #y =  np.ones(7)
-            #y[current_move[0]] = 0
-            #y = y/np.sum(y)
-            #y = th.tensor(y).unsqueeze(dim=0).unsqueeze(dim=0).unsqueeze(dim=0).float()
-
-            ## y_pred 
-            #current_state = th.tensor(game.history[turn-3]).unsqueeze(dim=0).unsqueeze(dim=0).float()
-            #y_pred = model(current_state)
-
-            #loss = criterion(y_pred, y)
- 
-            #loss_sum += loss.item()
-
-            #optimizer.zero_grad()
-            #loss.backward()
-            #optimizer.step()
+            if winner == 'Neural Network':
+                current_move = game.move_history[turn]
+                y =  np.ones(7) * 0.7
+                y[current_move[0]] = 0.2
+                y = th.tensor(y).unsqueeze(0).float()
+                current_state = th.tensor(game.history[turn]*(-1)).unsqueeze(dim=0).unsqueeze(dim=0).float()
+                y_pred = model(current_state)
+                loss = criterion(y_pred, y) 
+                loss_sum += loss.item() 
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                try:
+                    current_move = game.move_history[turn+1]
+                    y =  np.zeros(7)
+                    y[current_move[0]] = 1.0
+                    y = th.tensor(y).unsqueeze(0).float()
+                    current_state = th.tensor(game.history[turn+1]).unsqueeze(dim=0).unsqueeze(dim=0).float()
+                    y_pred = model(current_state)
+                    loss = criterion(y_pred, y) 
+                    loss_sum += loss.item() 
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                except IndexError:
+                    pass
+            elif winner == 'Draw':
+                pass  
         loss_arr.append(loss_sum/game.turn_counter)
-        print(f'Game {i} loss: {loss_sum/game.turn_counter:.3f}')
+        print(f'Game {i} loss: {loss_sum/game.turn_counter:.3f}, Winner {winner}')
 
 
-#loss_arr
-#fig, ax = plt.subplots(1,1)
-#ax.plot(loss_arr)
-#fig.savefig('loss.png', dpi=300)
-#
+fig, ax = plt.subplots(1,1)
+ax.plot(loss_arr)
+fig.savefig('loss.png', dpi=300)
+
+
 #game = Game()
 #
-#loss_arr[0]
+#nonzero_indices = np.nonzero(game.admissable_moves)[0] 
+#game.move(np.random.choice(nonzero_indices), -1) 
 #
-#game.move(0, 1)
-#game.state
 #state_input = th.tensor(game.state).unsqueeze(dim=0).unsqueeze(dim=0).float()
-#model_out = model(state_input*-1)
-#choice = (th.tensor(game.admissable_moves) * model_out).argmax().item()
-#game.move(choice, -1)
+#model_out = model(state_input)
+#
+#admissable_tens = th.tensor(game.admissable_moves)
+#nonzero_indices = th.nonzero(admissable_tens)
+#masked_tensor = model_out[0, nonzero_indices]
+#max_index = nonzero_indices[th.argmax(masked_tensor)]
+#choice = max_index.item()
+#game.move(choice, 1)
 #game.state
-
-
-
+#model_out
+#model_out.shape
--- model.py
+++ model.py
@@ -10,20 +10,16 @@ class CoolModel(nn.Module):
         """
         super().__init__()
         
-        output_dim = 7
-        # input_dim = 42 not necessary?
-        
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=5, stride=1) # output shape = 2x3
-        self.a1 = nn.ReLU()
-        
-        self.linear2 = nn.Linear(in_features=3, out_features=output_dim) # output 3x7
-        self.a2 = nn.ReLU()
-        
-        self.linear3 = nn.Linear(in_features=7, out_features=output_dim) # output 7x7
-        self.a3 = nn.ReLU()
-        
-        self.conv4 = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=(2,1), stride=1) # output shape = (1,1,1,7)
-        self.a4 = nn.Softmax(dim=3) # classes run over columns, columns correspond to dim 3
+        self.conv1 = th.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.relu1 = th.nn.ReLU()
+        self.conv2 = th.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.relu2 = th.nn.ReLU()
+        self.fc1 = th.nn.Linear(64 * 6 * 7, 64)
+        self.relu3 = th.nn.ReLU()
+        self.fc2 = th.nn.Linear(64, 64)
+        self.relu4 = th.nn.ReLU()
+        self.output = th.nn.Linear(64, 7)
+        self.relu_out = th.nn.ReLU()
 
     def forward(self, x):
         """
@@ -31,39 +27,11 @@ class CoolModel(nn.Module):
         a Tensor of output data. We can use Modules defined in the constructor as
         well as arbitrary operators on Tensors.
         """
-        x = self.conv1(x)
-        x = self.a1(x)
-        x = self.linear2(x)
-        x = self.a2(x)
-        x = self.linear3(x)
-        x = self.a3(x)
-        x = self.conv4(x)
-        x = self.a4(x)
+        x = self.relu1(self.conv1(x))
+        x = self.relu2(self.conv2(x))
+        x = x.view(x.size(0), -1)  # Flatten the feature maps
+        x = self.relu3(self.fc1(x))
+        x = self.relu4(self.fc2(x))
+        x = self.relu_out(self.output(x))
         return x
 
-# Create Tensors to hold input and outputs.
-#x = th.randint(low=-1, high=2, size=(6, 7))
-#x = x.unsqueeze(dim=0).unsqueeze(dim=0).float() # for batch dim
-#y = th.zeros(7).unsqueeze(dim=0).unsqueeze(dim=0)
-#
-## Construct our model by instantiating the class defined above
-#model = CoolModel()
-#
-## Construct our loss function and an Optimizer. The call to model.parameters()
-## in the SGD constructor will contain the learnable parameters (defined 
-## with torch.nn.Parameter) which are members of the model.
-#criterion = th.nn.MSELoss(reduction='sum')
-#optimizer = th.optim.SGD(model.parameters(), lr=0.1)
-#for t in range(5000):
-#    # Forward pass: Compute predicted y by passing x to the model
-#    y_pred = model(x)
-#
-#    # Compute and print loss
-#    loss = criterion(y_pred, y)
-#    if t % 100 == 99:
-#        print(t, loss.item())
-#
-#    # Zero gradients, perform a backward pass, and update the weights.
-#    optimizer.zero_grad()
-#    loss.backward()
-#    optimizer.step()
