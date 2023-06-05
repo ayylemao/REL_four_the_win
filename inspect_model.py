700f7bb56b68c97b37372f73c09cdaed57edcfaf
Karim <karim.ayadi@coop.ch>
2023-06-05 15:35:17 +0200

    plot kernels of first conv layer

--- inspect_model.py
+++ inspect_model.py
@@ -1,5 +1,22 @@
 '''
 Inspect learnt convolution kernels.
 '''
+import torch as th
+import matplotlib.pyplot as plt
 
 model = th.load("model.torch")
+
+# kernels of first conv layer
+nrows = 10
+ncols = 10
+fig, axs = plt.subplots(nrows, ncols)
+kernels = []
+counter = 0
+for i in range(nrows):
+    for j in range(ncols):
+        kernel = model.get("model_state_dict").get("conv1.weight")[counter].squeeze().squeeze()
+        axs[i,j].imshow(kernel)
+        kernels.append(kernel)
+        counter += 1
+plt.show()
+
