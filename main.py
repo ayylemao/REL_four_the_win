090ba786f457159aad7baed378248006d120b43f
Matthias Vogler <matthias.vogler@coop.ch>
2023-06-05 14:18:28 +0200

    alterating starting players

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import torch as th
 import matplotlib.pyplot as plt
 import random
 
-loss_move = -0.8
-win_move = +0.8
-neutral_move = 0.2
+loss_move = -0.4
+win_move = +0.6
+neutral_move = 0.0
 nonstarter_bonus = 0.0
 
 model_A = CoolModel()
 model_B = CoolModel()
-criterion = th.nn.MSELoss()
+criterion = th.nn.CrossEntropyLoss()
 optimizer_A = th.optim.SGD(model_A.parameters(), lr=0.0001)
 optimizer_B = th.optim.SGD(model_B.parameters(), lr=0.0001)
 prop_A = Propagation(model=model_A, 
@@ -37,61 +37,90 @@ wins_b = 0
 win_hist = []
 PLAYER_A = 1
 PLAYER_B = -1
-STARTER_DICT = {PLAYER_A : True, PLAYER_B : False}
 PROP_DICT = {PLAYER_A : prop_A, PLAYER_B : prop_B}
 for i in range(0, 100000):
     game = Game()
     nnplayer_A = NNPlayer(game=game, model=model_A)#RandomPlayer(game=game)
     nnplayer_B = NNPlayer(game=game, model=model_B)
 
-    
+    if i % 2 == 0: 
     # Game Loop
     # ================================= Game Loop ===================================
-    while True:
-        # Player 1
-        nnplayer_A.move(player=PLAYER_A)
-        
-        # win check 
-        if game.concluded:
-            if game.outcome != 'draw': 
-                winner = PLAYER_A
-                loser = PLAYER_B
-                wins_a += 1
-            elif game.outcome == 'draw':
-                print('Draw')
-            break
+        while True:
+            # Player 1
+            starter_dict = {PLAYER_A : True, PLAYER_B : False}
+            nnplayer_A.move(player=PLAYER_A)
+            
+            # win check 
+            if game.concluded:
+                if game.outcome != 'draw': 
+                    winner = PLAYER_A
+                    loser = PLAYER_B
+                    wins_a += 1
+                elif game.outcome == 'draw':
+                    print('Draw')
+                break
 
-        # Player 2
-        nnplayer_B.move(player=PLAYER_B) 
-        
-        
-        if game.concluded:
-            if game.outcome != 'draw': 
-                winner = PLAYER_B
-                loser = PLAYER_A
-                wins_b += 1
-            elif game.outcome == 'draw':
-                winner = 'Draw'
-                print('Draw')
-            break
+            # Player 2
+            nnplayer_B.move(player=PLAYER_B) 
+            
+            
+            if game.concluded:
+                if game.outcome != 'draw': 
+                    winner = PLAYER_B
+                    loser = PLAYER_A
+                    wins_b += 1
+                elif game.outcome == 'draw':
+                    winner = 'Draw'
+                    print('Draw')
+                break
+    if i % 2 == 1:
+        while True:
+            starter_dict = {PLAYER_A : False, PLAYER_B : True}
+            # Player 2
+            nnplayer_B.move(player=PLAYER_B) 
+            
+            
+            if game.concluded:
+                if game.outcome != 'draw': 
+                    winner = PLAYER_B
+                    loser = PLAYER_A
+                    wins_b += 1
+                elif game.outcome == 'draw':
+                    winner = 'Draw'
+                    print('Draw')
+                break
+
+            # Player 1
+            nnplayer_A.move(player=PLAYER_A)
+            
+            # win check 
+            if game.concluded:
+                if game.outcome != 'draw': 
+                    winner = PLAYER_A
+                    loser = PLAYER_B
+                    wins_a += 1
+                elif game.outcome == 'draw':
+                    print('Draw')
+                break
     # =================================================================================
-
     # Reenforcement Part
     if game.outcome != 'draw':
         win_loss = PROP_DICT[winner].rel_win_prop(game=game,
                                                   player=winner,
-                                                  starter=STARTER_DICT[winner])
+                                                  starter=starter_dict[winner])
         loss_loss = PROP_DICT[loser].rel_loss_prop(game=game,
                                                    player=loser,
-                                                   starter=STARTER_DICT[loser])
+                                                   starter=starter_dict[loser])
         win_arr.append(win_loss) 
         loss_arr.append(loss_loss)
+        win_hist.append(winner)
     #if i % 1 == 100:
         #recent_loss = loss_arr[-11:-1]
         #print(f'Game {i} loss: {np.mean(recent_loss):.3f}, Winner: {winner}')
     if i % 100 == 0 and i != 0:
-        mean_loss_loss = np.mean(loss_arr)
-        mean_win_loss = np.mean(win_arr)
+        mean_loss_loss = np.mean(loss_arr[-100:-1])
+        mean_win_loss = np.mean(win_arr[-100:-1])
         print(f'Game {i}:')
         print(f'Mean Win Loss: {mean_win_loss:.3f}')
         print(f'Mean Loss Loss: {mean_loss_loss:.3f}')
@@ -100,35 +129,25 @@ for i in range(0, 100000):
         print(f'Example State:')
         print(game.state)
         print(f'Win type: {game.outcome}, winner {winner}')
-        loss_arr = []
-        win_hist.append(winner)
         wins_a = 0
         wins_b = 0
 
 
-
-#fig, ax = plt.subplots(1,1)
-#ax.plot(win_hist)
-#fig.savefig('loss.png', dpi=300)
+fig, ax = plt.subplots(1,1)
+ax.plot(loss_arr)
+fig.savefig('loss.png', dpi=300)
 #
 #th.save(model.state_dict(), 'model001.torch')
 #
-#game = Game()
-#game.move(3, -1)
-#
-#state_input = th.tensor(game.state).unsqueeze(dim=0).unsqueeze(dim=0).float()
-#
-#model_out = model(state_input)
-#model_out
 #
 #
 #game = Game()
 #
 #nonzero_indices = np.nonzero(game.admissable_moves)[0] 
-#game.move(np.random.choice(nonzero_indices), -1) 
+#game.move(4, -1) 
 #
 #state_input = th.tensor(game.state).unsqueeze(dim=0).unsqueeze(dim=0).float()
-#model_out = model(state_input)
+#model_out = model_A(state_input)
 #
 #admissable_tens = th.tensor(game.admissable_moves)
 #nonzero_indices = th.nonzero(admissable_tens)
